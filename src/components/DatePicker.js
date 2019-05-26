7a7d34e4d4345080ca983f380a2b5465594a4577
Asif Nawaz <asif.saho@gmail.com>
2019-05-26 10:52:17 +0200

    datepicker component

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,33 +1,48 @@
-import React, {Component} from 'react';
-import {SingleDatePicker} from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
-import moment from 'moment';
+import React from "react";
+import DatePicker from "react-datepicker";
+import {withStyles} from '@material-ui/core';
+import "react-datepicker/dist/react-datepicker.css";
 
-class DatePicker extends Component {
-    constructor(props){
+class DatePickerWrap extends React.Component {
+    constructor(props) {
         super(props);
-
         this.state = {
-            date: moment(),
-            focused: false
-        }
+            date: ''
+        };
     }
 
-    onDateChange = (date) => {
-        this.setState({date});
-        this.props.handleDatePickerChange(this.props.name, date)
+    handleChange = (date) => {
+        this.setState({
+            date: date
+        });
+
+        this.props.handleChange({
+            target: {
+                value: date,
+                name: this.props.name
+            }
+        })
     };
 
     render() {
         return (
-            <SingleDatePicker
-                date={this.state.date}
-                onDateChange={this.onDateChange}
-                focused={this.state.focused}
-                onFocusChange={({focused}) => this.setState({focused})}
+            <DatePicker
+                className={this.props.classes.datePicker}
+                selected={this.state.date}
+                onChange={this.handleChange}
             />
-        )
+        );
     }
 }
 
-export default DatePicker;
+const styles = {
+    datePicker: {
+        border: 0,
+        borderBottom: '1px solid gray',
+        padding: '8px 0',
+        fontSize: '14px',
+        width: '100%'
+    }
+};
+
+export default withStyles(styles)(DatePickerWrap);
